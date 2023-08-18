fd108e09621e4cfb315dabbd33b4ca60c19434cc
Jheikkinen <remiraz@gmail.com>
2023-08-18 20:42:26 +0300

    Create anecdote.jsx

---
+++ osa1/anecdote.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const getRandomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+  const voteAnecdotes = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+  const mostVotedAnecdotes = votes.indexOf(Math.max(...votes))
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteAnecdotes}>vote </button>
+      <button onClick={getRandomAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVotedAnecdotes]}
+      <p>Has {votes[mostVotedAnecdotes]} votes</p>
+    </div>
+  )
+}
+
+export default App
